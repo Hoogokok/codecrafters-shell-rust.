f1ba4a2bd2cfd2d855af6220990305dabab92361
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-06-15 22:03:12 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,11 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    //<command_name>: command not found for an unrecognized command
+    let command = input.trim();
+    if command != "echo" {
+        eprintln!("{}: command not found", command);
+        std::process::exit(1);
+    }
 }
