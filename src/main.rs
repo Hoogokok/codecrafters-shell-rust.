22ae9755a0a8b2031a790403b665c016e76437e8
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-06-15 22:08:09 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,26 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        // Uncomment this block to pass the first stage
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
 
-    //<command_name>: command not found for an unrecognized command
-    let command = input.trim();
-    if command != "echo" {
-        eprintln!("{}: command not found", command);
-        std::process::exit(1);
+        //<command_name>: command not found for an unrecognized command
+        let command = input.trim();
+        match command {
+            "exit" => {
+                println!("Goodbye!");
+                return;
+            }
+            _ => {
+                eprintln!("{}: command not found", command);
+            }
+        }
     }
 }
