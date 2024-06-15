cd2cb9df65992d75fe21fc9117cdff2b9e241b48
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-06-15 22:23:38 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::ops::ControlFlow;
 
 fn main() {
     loop {
@@ -15,17 +16,49 @@ fn main() {
         //<command_name>: command not found for an unrecognized command
         let command = input.trim();
         let command_parts: Vec<&str> = deconstruct_command(command);
-        match command_parts[0] {
-            "exit 0" => {
-                return;
-            }
-            "echo" => {
-                println!("{}", command_parts[1..].join(" "));   
-            }
-            _ => {
-                eprintln!("{}: command not found", command);
-            }
+        let fun_name = process_command(command_parts, command);
+        if let ControlFlow::Break(_) = fun_name {
+            return;
         }
+    }
+}
+
+fn process_command(command_parts: Vec<&str>, command: &str) -> ControlFlow<()> {
+    match command_parts[0] {
+        "exit 0" => {
+            return ControlFlow::Break(());
+        }
+        "echo" => {
+            println!("{}", command_parts[1..].join(" "));   
+        }
+        "type" => {
+            identify_command(command_parts);
+        }
+        _ => {
+            eprintln!("{}: command not found", command);
+        }
+    }
+    ControlFlow::Continue(())
+}
+
+fn identify_command(command_parts: Vec<&str>) {
+    match command_parts[1] {
+        "exit" => {
+            println!("exit is a shell builtin");
+        }
+        "echo" => {
+            println!("echo is a shell builtin");
+        }
+        "type" => {
+            println!("type is a shell builtin");
+        }
+        "cat" => {
+            println!("cat is /bin/cat");
+        }
+        _ => {
+            println!("{}: not found", command_parts[1]);
+        }
+    
     }
 }
 // exit 0: exit the shell
